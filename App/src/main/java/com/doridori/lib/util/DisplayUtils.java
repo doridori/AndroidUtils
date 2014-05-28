3f5434bed841ac424ec19e4cc390705366095030
Dorian Cussen <github@doridori.co.uk>
2014-05-28 12:48:54 +0100

    display utils

--- App/src/main/java/com/doridori/lib/util/DisplayUtils.java
+++ App/src/main/java/com/doridori/lib/util/DisplayUtils.java
@@ -1,6 +1,7 @@
 package com.doridori.lib.util;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.DisplayMetrics;
 
 public class DisplayUtils
@@ -26,5 +27,27 @@ public class DisplayUtils
 
         float smallestWidth = Math.min(widthDp, heightDp);
         return smallestWidth;
+    }
+
+    public static float getCurrentOrientationWidth(Context context)
+    {
+        DisplayMetrics metrics = new DisplayMetrics();
+        context.getResources().getDisplayMetrics();
+
+        int widthPixels = metrics.widthPixels;
+        int heightPixels = metrics.heightPixels;
+
+        //height and width above do not changed based upon orientation so work out which one is the current length
+        int orientation = context.getResources().getConfiguration().orientation;
+        switch(orientation)
+        {
+            case Configuration.ORIENTATION_LANDSCAPE:
+                return Math.max(widthPixels, heightPixels);
+            case Configuration.ORIENTATION_PORTRAIT;
+                return Math.min(widthPixels, heightPixels);
+            default: //fallback
+                XLog.w("undefined orientation!");
+                return widthPixels;
+        }
     }
 }
