cf198f8c94414fd6c361dcfe076583addf2fddcc
Dorian Cussen <github@doridori.co.uk>
2014-09-23 15:27:00 +0100

    counter  improvement

--- App/src/main/java/com/doridori/lib/app/ActivityCounter.java
+++ App/src/main/java/com/doridori/lib/app/ActivityCounter.java
@@ -15,12 +15,19 @@
  */
 package com.doridori.lib.app;
 
+import android.os.Handler;
+
 import de.greenrobot.event.EventBus;
 
 /**
  * As there is no global callback for the state of the app i.e. is the app in the foreground or not you can have a base class
  * call this in onStart and onStop and it will do a global event for anyone who is interested. Useful for implementing some
  * form of security when the app moves to the foreground
+ *
+ * This works when moving from one activity to the next as onStart of the next activity is called before onStop of the current activity. However on rotation
+ * onStop is called before onStart of the next, which breaks this count. I have introduced a delay timer for after {@link #activityStopped()} is called to get around this.
+ *
+ * See http://stackoverflow.com/questions/4414171/how-to-detect-when-an-android-app-goes-to-the-background-and-come-back-to-the-fo for a discussion on this
  */
 public class ActivityCounter
 {
@@ -30,18 +37,48 @@ public class ActivityCounter
 
     private static final ActivityCounter sInstance = new ActivityCounter();
 
-    private ActivityCounter() { /*singleton*/ }
 
-    public static ActivityCounter getsInstance()
+    public static ActivityCounter getInstance()
     {
         return sInstance;
     }
 
     //==================================================================================================
-    // CLASS
+    // FIELDS
     //==================================================================================================
 
-    public int mStartedCount = 0;
+    private static final long BACKGROUND_EVENT_DELAY_MS = 2000;
+
+    private Handler mHandler;
+    private Runnable mActivityStoppedRunnable = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            mStartedCount--;
+
+            if(mStartedCount == 0)
+            {
+                EventBus.getDefault().post(new ApplicationMovedIntoBackgroundEvent());
+            }
+        }
+    };
+
+    private int mStartedCount = 0;
+
+
+    //==================================================================================================
+    // CONSTRUCTOR
+    //==================================================================================================
+
+    private ActivityCounter()
+    {
+        mHandler = new Handler();
+    }
+
+    //==================================================================================================
+    // LIFECYCLE HOOKS
+    //==================================================================================================
 
     public void activityStarted()
     {
@@ -53,13 +90,12 @@ public class ActivityCounter
 
     public void activityStopped()
     {
-        mStartedCount--;
-
-        if(mStartedCount == 0)
-        {
-            EventBus.getDefault().post(new ApplicationMovedIntoBackgroundEvent());
-        }
+        mHandler.postDelayed(mActivityStoppedRunnable, BACKGROUND_EVENT_DELAY_MS);
     }
+
+    //==================================================================================================
+    // EVENTS
+    //==================================================================================================
 
     public class ApplicationMovedIntoBackgroundEvent{}
     public class ApplicationMovedIntoForegroundEvent{}
